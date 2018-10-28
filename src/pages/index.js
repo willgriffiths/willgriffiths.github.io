0686a5ce00d4c57fe98c6442172402b936519b2d
Will Griffiths <thiswill@gmail.com>
2018-10-28 23:06:30 +1000

    Render index of tips from md

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,95 +43,120 @@ const Section = styled.section`
   margin-bottom: 58px;
 `;
 
-const IndexPage = ({
-  data: {
-    allMarkdownRemark: { edges }
-  }
-}) => (
-  <Container>
-    <Section>
-      <Title id="work">Work</Title>
-      <Text>Frontend Developer</Text>
-      <Text>Fusion Sport</Text>
-      <Text>Building the Human Performance Framework</Text>
-    </Section>
-    <Section>
-      <Title id="now">What am I doing now?</Title>
-      <Text>
-        Making this site less{" "}
-        <span role="img" aria-label="crappy">
-          💩
-        </span>
-      </Text>
-    </Section>
-    <Section>
-      <Title id="projects">Side stuff</Title>
-      <Projects edges={edges} />
-    </Section>
-    <Section>
-      <Title id="quick-tips">Quick tips</Title>
-      <Text>VS Code - insert sequence</Text>
-      <Text>VS Code - toggleFind</Text>
-      <Text>Git - The no commit cherry pick</Text>
-    </Section>
-    <Section>
-      <Title id="codepens">Pens</Title>
-      <Text>[Placeholder]</Text>
-    </Section>
-    <Section>
-      <Title id="tech">What I do</Title>
-      <Text>Javascript</Text>
-      <Text>React</Text>
-      <Text>HTML/CSS</Text>
-      <Text>Mobx and Mobx State Tree</Text>
-      <Text>React Native</Text>
-      <Text>Electron</Text>
-      <Text>Gatsby ;)</Text>
-      <Text>Jest</Text>
-      <Text>Eslint and Prettier</Text>
-      <Text>Circle CI</Text>
-      <Text>Git</Text>
-    </Section>
-    <Section>
-      <Title id="tools">Tools I love</Title>
-      <Text>VS Code</Text>
-      <Text>iTerm2</Text>
-      <Text>Alfred</Text>
-      <Text>Figma</Text>
-      <Text>Karabiner-Elements</Text>
-      <Text>nvAlt</Text>
-      <Text>Spectacle</Text>
-      <Text>Spotify</Text>
-      <Text>Taurine</Text>
-      <Text>Oh My Zsh</Text>
-      <Text>Meslo LG M DZ for Powerline</Text>
-      <Text>Antigen</Text>
-      <Text>Spaceship ZSH Theme</Text>
-    </Section>
-    <Section>
-      <Title id="contact">Contact</Title>
-      <ContactLink href="mailto:thiswill@gmail.com">thiswill@gmail.com</ContactLink>
-      <ContactLink href="https://github.com/willgriffiths">github.com/willgriffiths</ContactLink>
-      <ContactLink href="https://linkedin.com/in/williamgriffiths">
-        linkedin.com/in/williamgriffiths
-      </ContactLink>
-    </Section>
-  </Container>
-);
+const IndexPage = ({ data }) => {
+  const { ProjectsData, TipsData } = data;
+  return (
+    <Container>
+      <Section>
+        <Title id="work">Work</Title>
+        <Text>Frontend Developer</Text>
+        <Text>Fusion Sport</Text>
+        <Text>Building the Human Performance Framework</Text>
+      </Section>
+      <Section>
+        <Title id="now">What am I doing now?</Title>
+        <Text>
+          Making this site less{" "}
+          <span role="img" aria-label="crappy">
+            💩
+          </span>
+        </Text>
+      </Section>
+      <Section>
+        <Title id="projects">Side stuff</Title>
+        <Projects edges={ProjectsData.edges} />
+      </Section>
+      <Section>
+        <Title id="quick-tips">Quick tips</Title>
+        <Tips edges={TipsData.edges} />
+      </Section>
+      <Section>
+        <Title id="codepens">Pens</Title>
+        <Text>[Placeholder]</Text>
+      </Section>
+      <Section>
+        <Title id="tech">What I do</Title>
+        <Text>Javascript</Text>
+        <Text>React</Text>
+        <Text>HTML/CSS</Text>
+        <Text>Mobx and Mobx State Tree</Text>
+        <Text>React Native</Text>
+        <Text>Electron</Text>
+        <Text>Gatsby ;)</Text>
+        <Text>Jest</Text>
+        <Text>Eslint and Prettier</Text>
+        <Text>Circle CI</Text>
+        <Text>Git</Text>
+      </Section>
+      <Section>
+        <Title id="tools">Tools I love</Title>
+        <Text>VS Code</Text>
+        <Text>iTerm2</Text>
+        <Text>Alfred</Text>
+        <Text>Figma</Text>
+        <Text>Karabiner-Elements</Text>
+        <Text>nvAlt</Text>
+        <Text>Spectacle</Text>
+        <Text>Spotify</Text>
+        <Text>Taurine</Text>
+        <Text>Oh My Zsh</Text>
+        <Text>Meslo LG M DZ for Powerline</Text>
+        <Text>Antigen</Text>
+        <Text>Spaceship ZSH Theme</Text>
+      </Section>
+      <Section>
+        <Title id="contact">Contact</Title>
+        <ContactLink href="mailto:thiswill@gmail.com">thiswill@gmail.com</ContactLink>
+        <ContactLink href="https://github.com/willgriffiths">github.com/willgriffiths</ContactLink>
+        <ContactLink href="https://linkedin.com/in/williamgriffiths">
+          linkedin.com/in/williamgriffiths
+        </ContactLink>
+      </Section>
+    </Container>
+  );
+};
 
 export default IndexPage;
 
-const Projects = ({ edges }) => (
-  <div>
-    {edges.map((edge) => (
-      <Project key={edge.node.path} frontmatter={edge.node.frontmatter} html={edge.node.html} />
-    ))}
-  </div>
-);
+const Projects = ({ edges }) =>
+  edges && (
+    <div>
+      {edges.map((edge) => (
+        <Project key={edge.node.path} frontmatter={edge.node.frontmatter} html={edge.node.html} />
+      ))}
+    </div>
+  );
+
+const Tips = ({ edges }) =>
+  edges && (
+    <div>
+      {edges.map((edge) => <Text key={edge.node.path}>{edge.node.frontmatter.title}</Text>)}
+    </div>
+  );
 
 export const pageQuery = graphql`
-  query allMarkdown {
-    allMarkdownRemark(filter: { fileAbsolutePath: { glob: "**/projects/**" } }) {
+  query Index {
+    ProjectsData: allMarkdownRemark(filter: { fileAbsolutePath: { glob: "**/projects/**" } }) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+            date
+            tech
+            industry
+          }
+          tableOfContents
+          wordCount {
+            paragraphs
+            sentences
+            words
+          }
+          timeToRead
+        }
+      }
+    }
+    TipsData: allMarkdownRemark(filter: { fileAbsolutePath: { glob: "**/quick-tips/**" } }) {
       edges {
         node {
           frontmatter {
---
+++ src/pages/quick-tips/vs-code-toggle-find.md
@@ -0,0 +1,17 @@
+---
+path: "/quick-tips/vs-code-toggle-find"
+date: "2017-11-07"
+title: "VS Code - Toggle Find"
+tags: "vs code, multi-cursor"
+---
+
+I've been using vs-code's multi cursor extensively. It's super powerful but I always found myself trying to matching too many variables or component. Well, not anymore!
+
+( CMD + ) Alt + C for case sensitive toggleFindCaseSensitive
+( CMD + ) Alt + W for match whole word toggleFindWholeWord
+( CMD + ) Alt + R for using regular expressions toggleFindRegex. Is it meaningful to use regular expressions to find occurrences? I'm not sure but I'm glad it's a thing.
+
+image
+Image
+coverImage
+images
