72e5f37e55653bd1c0f047ff4ac585583b56957c
Will Griffiths <thiswill@gmail.com>
2018-11-18 18:03:31 +1000

    Improve project typography

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,25 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { graphql } from "gatsby";
-import styled from "styled-components";
 import Layout from "../layouts";
-import Container from "../components/Container";
-
-const Text = styled.p`
-  font-weight: 400;
-  margin-bottom: 12px;
-`;
+import { Container, Section } from "../components/FlexGrid";
+import Title from "../components/Title";
+import Text from "../components/Text";
 
 const ProjectTemplate = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark;
   return (
     <Layout>
       <Container>
-        <Text>{frontmatter.date && frontmatter.date.slice(0, 4)}</Text>
-        <Text>{frontmatter.title}</Text>
-        {frontmatter.industry && <Text>Industry: {frontmatter.industry}</Text>}
-        {frontmatter.tech && <Text>Tech: {frontmatter.tech}</Text>}
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <Section>
+          <Title>{frontmatter.title}</Title>
+          <Text>
+            {frontmatter.date && `Year: ${frontmatter.date.slice(0, 4)}`}
+          </Text>
+          {frontmatter.industry && (
+            <Text>Industry: {frontmatter.industry}</Text>
+          )}
+          {frontmatter.tech && <Text>Tech: {frontmatter.tech}</Text>}
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </Section>
       </Container>
     </Layout>
   );
