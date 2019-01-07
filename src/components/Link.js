5d97159290a97ba647438e87b53e9e8c9cf3f747
Will Griffiths <thiswill@gmail.com>
2019-01-07 23:50:44 +1000

    Style hyperlinks

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 import { Link } from "gatsby";
+import { lighten } from "polished";
+import theme from "../styles/colors";
 
 export default styled(Link)`
   text-decoration: none;
   font-weight: 400;
-  color: white;
+  color: ${theme.secondary};
   display: block;
   margin-bottom: 12px;
+  :visited {
+    color: ${lighten(0.05, theme.secondary)};
+  }
+  :hover {
+    color: ${theme.secondary};
+  }
+  :active {
+    color: ${lighten(0.1, theme.secondary)};
+  }
 `;
