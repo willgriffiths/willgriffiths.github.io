6bdb689a0dd9267ed8ec036f99ef581d4c752841
Will Griffiths <thiswill@gmail.com>
2019-01-09 20:41:53 +1000

    Add 404 page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Text from "../components/Text";
+import Title from "../components/Title";
+import { Container } from "../components/FlexGrid";
+import Layout from "../layouts";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <Container>
+      <Title>NOT FOUND</Title>
+      <Text>This page doesn&apos;t exist... circle back and try again.</Text>
+    </Container>
+  </Layout>
 );
 
 export default NotFoundPage;
